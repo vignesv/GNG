db14bba3ac774c41b6b74c65dc20fe80d8b00df6
vignesv <43526229+vignesv@users.noreply.github.com>
2018-10-02 13:24:22 -0400

    Update gng.py

--- gng.py
+++ gng.py
@@ -1,1 +1,31 @@
+import random as random
+from scipy.spatial import distance
+import numpy as numpy
+import matplotlib.pyplot as plt
 
+class gas:
+  
+  def __init__(self,position,nodeID,dist,error,neighbors):
+    self.position=position
+    self.nodeId=nodeID
+    self.dist=dist
+    self.error=error
+    self.neighbors=neighbors
+  
+  getPosition(self):
+    reuturn(self.Position)
+  getNodeID(self):
+    reuturn(self.nodeID)
+  getError(self):
+    reuturn(self.error)
+  getNeighbors(self):
+    reuturn(self.neighbors)
+  
+  setPosition(self,position):
+    self.position=position
+  setNodeID(self,nodeID):
+    self.nodeID=nodeID
+  setError(self,error):
+    self.error=error
+  setNeighbors(self,neighbor):
+    self.neighbor=neighbor
